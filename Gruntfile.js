97326c42f7241197025b5d833682557a67cb8dd6
bmac <bmac325@GMail.com>
2013-11-11 15:54:24 -0500

    Demo the training reporter task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,19 @@
 module.exports = function(grunt) {
+
+var previous_force_state = grunt.option('force');
+
+grunt.registerTask('force',function(set){
+    if (set === 'on') {
+        grunt.option('force',true);
+    }
+    else if (set === 'off') {
+        grunt.option('force',false);
+    }
+    else if (set === 'restore') {
+        grunt.option('force',previous_force_state);
+    }
+});
+
   grunt.initConfig({
     jshint: {
       all: [
@@ -20,6 +35,19 @@ module.exports = function(grunt) {
           livereload: true
         }
       }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          force: true,
+          reporter: 'json',
+          captureFile: 'out.json'
+        },
+        src: ['tests/runner.js']
+      }
+    },
+    reporter: {
+      all: {}
     }
   });
 
@@ -30,7 +58,9 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-training-reporter');
 
-  grunt.registerTask('default', [ 'jshint' ]);
+  grunt.registerTask('default', [ 'jshint', 'force:on', 'mochaTest', 'force:restore', 'reporter' ]);
   grunt.registerTask('develop', [ 'server', 'watch' ]);
 };
